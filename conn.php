54da89f63ac9a88d4912e2c04e844aec68579086
adegifari <adegifari40@gmail.com>
2018-11-27 12:52:07 +0700

    init

---
+++ conn.php
@@ -0,0 +1,9 @@
+<?php
+
+	$engi = "mysql";
+	$host =	"localhost";
+	$dbse = "blogs_2";
+	$user = "root";
+	$pass = "";
+
+	$koneksi = new PDO($engi.':dbname='.$dbse.";host=".$host, $user, $pass);
---
+++ home.php
@@ -0,0 +1,1 @@
+<h1>Welcome My BLOG !!!</h1>
